7faaaf6828a00e73c4b57c838bb6f69ebd85a126
Tom Villegas <tvillega@mailbox.org>
2026-07-09 16:18:49 -0400

    refactor: move edit logic to lib

--- libexec/e2-edit.sh
+++ libexec/e2-edit.sh
@@ -3,14 +3,33 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+check_dir() {
+
+  local wht="${1-}"
+  local lab="${2-}"
+
+  if [[ -z "$lab" ]] ; then
+    echo "usage: edit $wht <lab>"
+    exit
+  elif [[ ! -d "$lab" ]] ; then
+    echo "Laboratory $lab not found"
+    exit
+  elif [[ "$lab" == "labs" ]] ; then
+    echo "Can't use root directory as laboratory"
+    exit
+  fi  
+
+}
+
 edit_services() {
+  check_dir "services" "${1-}"
   $EDITOR "${1%%/}/docker-compose-services.yml"
 }
 
 edit_networks() {
+  check_dir "networks" "${1-}"
   $EDITOR "${1%%/}/docker-compose-networks.yml"
 }
-
 
 [[ -z "${1-}" ]] && exit
 
