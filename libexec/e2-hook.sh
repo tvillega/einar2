91091f2a57d8bb66b73313dac8c34d72f8f65cf9
Tom Villegas <tvillega@mailbox.org>
2026-07-09 14:32:35 -0400

    feat: add default hooks

--- libexec/e2-hook.sh
+++ libexec/e2-hook.sh
@@ -2,16 +2,18 @@
 
 hook_run() {
 
-  if [[ $1 -lt 0 ]] ; then
+  local s="$1" ; shift
+
+  if [[ $s -lt 0 ]] ; then
     for a in config/hooks/*pre.run ; do
       chmod +x $a
-      bash $a
+      $a "${@}"
       chmod -x $a
     done
   else
     for a in config/hooks/*post.run ; do
       chmod +x $a
-      bash $a
+      $a "${@}"
       chmod -x $a
     done
   fi
@@ -20,16 +22,18 @@ hook_run() {
 
 hook_edit() {
 
-  if [[ $1 -lt 0 ]] ; then
+  local s="$1" ; shift
+
+  if [[ $s -lt 0 ]] ; then
     for a in config/hooks/*pre.edit ; do
       chmod +x $a
-      bash $a
+      $a "${@}"
       chmod -x $a
     done
   else
     for a in config/hooks/*post.edit ; do
       chmod +x $a
-      bash $a
+      $a "${@}"
       chmod -x $a
     done
   fi
@@ -38,37 +42,42 @@ hook_edit() {
 
 hook_gen() {
 
-  if [[ $1 -lt 0 ]] ; then
+  local s="$1" ; shift
+
+  if [[ $s -lt 0 ]] ; then
     for a in config/hooks/*pre.gen ; do
       chmod +x $a
-      bash $a
+      $a "${@}"
       chmod -x $a
     done
   else
     for a in config/hooks/*post.gen ; do
       chmod +x $a
-      bash $a
+      $a "${@}"
       chmod -x $a
     done
   fi
 
 }
 
+[[ ! -d "config/hooks" ]] && exit
+
 [[ -z "${1-}" ]] && exit
 
 while [[ "$1" != "--" ]]; do case $1 in
   run)
     shift
-    hook_run $2
+    hook_run "${@}"
     exit
     ;;
   edit)
     shift
-    hook_edit $2
+    hook_edit "${@}"
     exit
     ;;
   gen)
-    hook_gen $2
+    shift
+    hook_gen "${@}"
     exit
     ;;
 esac; shift; done
