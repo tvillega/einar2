22c2b62010f51d7b333d85f8e820b8614b7f17b9
Tom Villegas <tvillega@mailbox.org>
2026-07-12 00:27:42 -0400

    feat: add docker netns linker to host

---
+++ config/includes.script/hijack-netns.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+## Finds the reserved namespace location /run/docker/netns
+## and links it to the global /run/netns
+
+echo "-> ip net list"
+ip net list
+
+echo ":: Gathering container names"
+CONTAINERS=( $( docker ps --format "table {{ .Names }}") )
+CONTAINERS=${C[@]:1} # Remove column name
+
+for c in "${CONTAINERS[@]}" ; do
+  echo $c
+done
+
+echo ":: Getting containers PIDs"
+
+PIDS=()
+for c in "${CONTAINERS[@]}" ; do
+  P=( $(docker inspect --format '{{ .State.Pid }}' $c) )
+  PIDS+=( $P )
+  echo "$c -> $P"
+done
+
+echo ":: Hijacking network namespaces"
+for c in "${CONTAINERS[@]}" ; do
+  ip netns attach $c ${P[0]}
+done
+
+echo "-> ip net list"
+ip net list
