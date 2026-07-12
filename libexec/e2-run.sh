4da8997183b808089691f726afd6d5526ac24de6
Tom Villegas <tvillega@mailbox.org>
2026-07-12 01:28:30 -0400

    feat: add pre|post start|stop hooks

--- libexec/e2-run.sh
+++ libexec/e2-run.sh
@@ -16,7 +16,9 @@ run_start()     {
     docker compose -f "compose/docker-compose-registry.yml" up -d
 
   else
+    ./libexec/e2-hook.sh start -1 "${@}"
     docker compose -f "${1%%/}/docker-compose-services.yml" up --build
+    ./libexec/e2-hook.sh start  1 "${@}"
   fi
 }
 
@@ -32,7 +34,9 @@ run_stop()      {
     docker compose -f "compose/docker-compose-registry.yml" down
 
   else
+    ./libexec/e2-hook.sh stop -1 "${@}"
     docker compose -f "${1%%/}/docker-compose-services.yml" down
+    ./libexec/e2-hook.sh stop  1 "${@}"
   fi
 }
 
