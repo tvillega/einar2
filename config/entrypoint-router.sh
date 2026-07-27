4d3a5dfa3a52874fd31c9672a24a532dc0364d11
Tom Villegas <tvillega@mailbox.org>
2026-07-27 01:18:11 -0400

    fix: router containers crash on first start

--- config/entrypoint-router.sh
+++ config/entrypoint-router.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
+
+quagga_dir="/etc/quagga"
+zebra_file="${quagga_dir}/zebra.conf"
+daemons_file="${quagga_dir}/daemons"
+
+echo ":: Starting sanity checks for quagga"
+
+echo ":: Checking existence of zebra.conf"
+sleep 1
+
+if [[ ! -e "$zebra_file" ]] ; then
+  echo "  -> Not found, creating empty file"
+  touch "$zebra_file"
+
+else
+  echo "  -> File zebra.conf found, importing configurations"
+fi
+
+echo ":: Checking existence of daemons file"
+sleep 1
+
+if [[ ! -e "$daemons_file" ]] ; then
+  echo "  -> Not found, creating default file"
+  echo "zebra=yes" > /etc/quagga/daemons
+
+else
+  echo "  -> File daemons found, importing configurations"
+fi
+
+echo ":: Sanity checks completed, dropping to quagga"
+
 exec /usr/sbin/zebra
