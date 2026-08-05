df8de284234c7da42ca569d8d208a75152a8043e
Tom Villegas <tvillega@mailbox.org>
2026-08-04 23:06:20 -0400

    feat: improve sync between prod and devel env

--- utils/sync.sh
+++ utils/sync.sh
@@ -6,15 +6,73 @@
 # which simulate where they would be available
 # inside the published Einar2 docker image
 
-if [[ ! -d www/archetypes ]] ; then
-  mkdir -v www/archetypes
+## SYNC ARCHETYPES
+
+archdir="www/archetypes"
+
+if [[ ! -d "$archdir" ]] ; then
+  mkdir -v "$archdir"
 fi
 
-cp -rv --remove-destination archetypes/*    www/archetypes/
+cp -rv --remove-destination archetypes/*  "${archdir}/"
 
-if [[ ! -d www/docs/md ]] ; then
-  mkdir -v www/docs/md
+## SYNC DOCS (ROOT)
+
+docsdir="www/docs/md"
+
+if [[ ! -d "$docsdir" ]] ; then
+  mkdir -v "$docsdir"
 fi
 
-cp -rv --remove-destination docs/*          www/docs/md/
-cp -v  --remove-destination CHANGELOG.md  www/docs/md/releasenotes.md
+cp -rv --remove-destination docs/index.md  "${docsdir}/index.md"
+cp -v  --remove-destination CHANGELOG.md   "${docsdir}/releasenotes.md"
+
+## SYNC USER GUIDE
+
+guidedir="www/docs/userguide/md"
+
+if [[ ! -d "$guidedir" ]] ; then
+  mkdir -v "$guidedir"
+fi
+
+cp -rv --remove-destination docs/userguide/* "${guidedir}/"
+
+## SYNC ADVANCED USER GUIDE
+
+advguidedir="www/docs/advguide/md"
+
+if [[ ! -d "$advguidedir" ]] ; then
+  mkdir -v "$advguidedir"
+fi
+
+cp -rv --remove-destination docs/advguide/* "${advguidedir}/"
+
+## SYNC CONTACT
+
+contactdir="www/docs/contact/md"
+
+if [[ ! -d "$contactdir" ]] ; then
+  mkdir -v "$contactdir"
+fi
+
+cp -rv --remove-destination docs/contact/* "${contactdir}/"
+
+## SYNC DOWNLOAD
+
+downloaddir="www/docs/download/md"
+
+if [[ ! -d "$downloaddir" ]] ; then
+  mkdir -v "$downloaddir"
+fi
+
+cp -rv --remove-destination docs/download/* "${downloaddir}/"
+
+## SYNC LINKS
+
+linksdir="www/docs/links/md"
+
+if [[ ! -d "$linksdir" ]] ; then
+  mkdir -v "$linksdir"
+fi
+
+cp -rv --remove-destination docs/links/* "${linksdir}/"
