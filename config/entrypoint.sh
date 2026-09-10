70299c1f0496bcfb163e9fe18be0e976b39a068c
Tom Villegas <tvillega@mailbox.org>
2026-09-10 00:44:19 -0300

    feat: centralize config dir

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-exec lighttpd -D -f /config/lighttpd.d/einar2.conf
+
+if [[ -z "$(ls -A /config)" ]]; then
+  cp -r /config.default/* /config/
+  chown -R lighttpd:lighttpd /config/www.d
+  chown -R lighttpd:lighttpd /config/archetype.d
+  chmod +x config/entrypoint.d/*.sh
+fi
+
+ln -sf /config/archetype.d /einar2/archetypes # because PHP reads raw filesystem
+
+exec lighttpd -D -f /config.default/lighttpd.conf
